77c97a00ca82f57217ce2f1f50293769c1c7a920
dev-campos <dev_campos@hotmail.com>
2024-01-12 10:29:03 -0300

    Fix file naming and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { THEME, TonConnectUIProvider } from "@tonconnect/ui-react";
 import { Header } from "./components/Header/Header";
 import "./App.css";
