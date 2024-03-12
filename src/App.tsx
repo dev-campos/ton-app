08809671a883ea7c600fde4c96ad1cc22f399614
dev-campos <dev_campos@hotmail.com>
2024-03-12 09:41:51 -0300

    Coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
 import styles from "./App.module.scss";
-import { useEffect } from "react";
-import { Header } from "./components/Layout/Header/Header";
-import { useAppDispatch } from "./hooks/reduxHooks";
-
-import { Outlet } from "react-router-dom";
-import Loader from "./components/Loader/Loader";
-import { useGetActiveAssetPairsQuery } from "./services/owlsApiSlice";
-import { changeActivePair, changePairs } from "./features/pairsSlice";
 
 function App() {
-    const dispatch = useAppDispatch();
-    const { data: pairsData, isLoading: pairsIsLoading } =
-        useGetActiveAssetPairsQuery(null);
-
-    useEffect(() => {
-        if (pairsData) {
-            dispatch(changeActivePair(pairsData.data[0]));
-            dispatch(changePairs(pairsData.data));
-        }
-    }, [dispatch, pairsData]);
-
-    return (
-        <div className={styles.app}>
-            <Header />
-            {pairsIsLoading ? <Loader /> : <Outlet />}
-        </div>
-    );
+    return <div className={styles.comingSoon}></div>;
 }
 
 export default App;
